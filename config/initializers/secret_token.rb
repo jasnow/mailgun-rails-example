4f2ba552edd1428acb9ec4d47378598f604bab1f
Al Snow <jasnow@hotmail.com>
2016-06-13 11:42:46 -0400

    Clean up some errors

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SendEmailRuby::Application.config.secret_key_base = 'f2eaa372357642e5769dee30d498158d4e3ee699a94cb26e48df7294b1494e0104bccb18a0384768215149547c503fd59cdb7c37c3ca8884111a80dd48f92405'
+SendEmailRuby::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+
